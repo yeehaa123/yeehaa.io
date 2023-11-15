264597ac7944774ac6a251123d55d438fe297e69
Jan Hein Hoogstad <yeehaa@offcourse.io>
2023-11-15 14:20:21 +0100

    breakout colorScheme

---
+++ styles/colorSchemes/TaipeiBuilding.js
@@ -0,0 +1,72 @@
+const turquoise = {
+	50: '#f3faf9',
+	100: '#d6f1ed',
+	200: '#ace3d9',
+	300: '#7acec2',
+	400: '#4fb2a8',
+	500: '#35978e',
+	600: '#287973',
+	700: '#24615d',
+	800: '#214e4c',
+	900: '#1f4241',
+	950: '#010303',
+};
+
+const sun = {
+	50: '#fdffe7',
+	100: '#faffc1',
+	200: '#f9ff86',
+	300: '#feff41',
+	400: '#fff50d',
+	500: '#ffe700',
+	600: '#d1ab00',
+	700: '#a67b02',
+	800: '#89600a',
+	900: '#744e0f',
+	950: '#442a04',
+};
+
+const ice = {
+	50: '#f2f6fc',
+	100: '#e2ecf7',
+	200: '#cbddf2',
+	300: '#a7c8e9',
+	400: '#7eabdc',
+	500: '#5487cf',
+	600: '#4b75c5',
+	700: '#4163b4',
+	800: '#3a5293',
+	900: '#334675',
+	950: '#232c48',
+};
+
+const violet = {
+	50: '#f2f2fd',
+	100: '#ececfb',
+	200: '#dbdbf9',
+	300: '#c1bff3',
+	400: '#a09aeb',
+	500: '#8071e1',
+	600: '#6c52d5',
+	700: '#5c40c1',
+	800: '#4d35a2',
+	900: '#402d85',
+	950: '#261b5a',
+
+};
+
+const colors = {
+	black: turquoise['950'],
+	offblack: turquoise['900'],
+	primary: sun['500'],
+	'primary-light': sun['400'],
+	'primary-dark': sun['600'],
+	secondary: violet['700'],
+	'secondary-light': violet['400'],
+	'secondary-dark': violet['800'],
+	tertiary: ice['500'],
+	white: ice['50'],
+	offwhite: ice['100'],
+};
+
+export default colors;
