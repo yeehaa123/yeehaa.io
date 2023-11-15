7a4aa21b88fdef4959573d8f8da3e9cb71837118
Jan Hein Hoogstad <yeehaa@offcourse.io>
2023-11-15 10:00:57 +0100

    colorsgit add -A!

---
+++ colors.js
@@ -0,0 +1,19 @@
+const turquoise =  {
+    50: '#f3faf9',
+    100: '#d6f1ed',
+    200: '#ace3d9',
+    300: '#7acec2',
+    400: '#4fb2a8',
+    500: '#35978e',
+    600: '#287973',
+    700: '#24615d',
+    800: '#214e4c',
+    900: '#1f4241',
+    950: '#010303',
+};
+
+export default colors = {
+  turquoise,
+  black: turquoise.950,
+  offblack: turquoise.900,
+};
