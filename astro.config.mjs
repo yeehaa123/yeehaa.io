22d37104b536e2f6015d3f460ba107655376f53d
Jan Hein Hoogstad <yeehaa@offcourse.io>
2023-11-15 16:50:49 +0100

    footnotes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import { visit } from 'unist-util-visit';
 import section from '@hbsnow/rehype-sectionize';
 import unwrapImages from 'remark-unwrap-images';
+import remarkGfm from 'remark-gfm'
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 
@@ -11,7 +12,7 @@ import alpinejs from "@astrojs/alpinejs";
 export default defineConfig({
   experimental: {},
   markdown: {
-    remarkPlugins: [unwrapImages],
+    remarkPlugins: [unwrapImages, remarkGfm],
     rehypePlugins: [section]
   },
   integrations: [tailwind(), mdx(), alpinejs()]
