2f7235817cbf44b7145362aed091312cf7ba0646
Jan Hein Hoogstad <yeehaa@offcourse.io>
2023-11-18 19:08:01 +0100

    add typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,20 @@
-import { defineConfig } from 'astro/config';
-import { visit } from 'unist-util-visit';
+import {defineConfig} from 'astro/config';
+import {visit} from 'unist-util-visit';
 import section from '@hbsnow/rehype-sectionize';
+import classNames from 'rehype-class-names';
+import slug from 'rehype-slug';
 import unwrapImages from 'remark-unwrap-images';
-import remarkGfm from 'remark-gfm'
+import remarkGfm from 'remark-gfm';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
-
-import alpinejs from "@astrojs/alpinejs";
+import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {},
-  markdown: {
-    remarkPlugins: [unwrapImages, remarkGfm],
-    rehypePlugins: [section]
-  },
-  integrations: [tailwind(), mdx(), alpinejs()]
+	experimental: {},
+	markdown: {
+		remarkPlugins: [unwrapImages, remarkGfm],
+		rehypePlugins: [[classNames, {'h1 + p': 'lead'}], slug, section],
+	},
+	integrations: [tailwind(), mdx(), alpinejs()],
 });
