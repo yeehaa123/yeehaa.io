22310c19b27508aab13fe8b11d1627248274a7db
Jan Hein Hoogstad <yeehaa@offcourse.io>
2023-11-22 11:10:29 +0100

    add print styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme');
+const twColors = require('tailwindcss/colors');
 const colors = require('./styles/colorSchemes/BambooCurtain');
 
 export default {
@@ -7,7 +8,12 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			colors,
+			colors: {
+				'pitch-black': twColors.black,
+				'pure-white': twColors.white,
+				...colors,
+
+			},
 			fontFamily: {
 				mono: ['Nitti', ...defaultTheme.fontFamily.sans],
 				sans: ['GT Ultra Standard', ...defaultTheme.fontFamily.sans],
