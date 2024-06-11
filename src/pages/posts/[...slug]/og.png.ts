0abe8bb22e396257118f108f7512d41a425c95d1
Jan Hein Hoogstad <yeehaa@offcourse.io>
2024-06-11 19:01:04 +0200

    resize og image

--- src/pages/posts/[...slug]/og.png.ts
+++ src/pages/posts/[...slug]/og.png.ts
@@ -1,7 +1,7 @@
 import type { APIRoute } from "astro";
-import { readFile } from "fs/promises";
 import { getCollection } from "astro:content";
 import path from "path";
+import sharp from "sharp";
 
 export async function getStaticPaths() {
   const blogEntries = await getCollection('Posts');
@@ -9,14 +9,16 @@ export async function getStaticPaths() {
     params: { slug: entry.slug }, props: { entry },
   }));
 }
+
 export const GET: APIRoute = async function get({ props }) {
-  const postCover = await readFile(
+  const postCover = await sharp(
     process.env.NODE_ENV === 'development'
       ? path.resolve(
         props.entry.data.imageURL.src.replace(/\?.*/, '').replace('/@fs', ''),
       )
       : path.resolve(props.entry.data.imageURL.src.replace('/', 'dist/')),
-  );
+  ).resize(1200).toBuffer()
+
   return new Response(postCover, {
     headers: {
       "Content-Type": "image/png",
