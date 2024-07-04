deb8c6e85f89868eac9f1d63d48777a262dd1824
Jan Hein Hoogstad <yeehaa@offcourse.io>
2024-07-04 10:08:05 +0200

    improve rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,12 +2,16 @@ import rss from '@astrojs/rss';
 import { getPosts } from "@/helpers/pages";
 import { getEntry } from "astro:content";
 import type { AstroUserConfig } from 'astro/config';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
+
 
 export async function GET(context: AstroUserConfig) {
   const title = "YEEHAA";
   const posts = await getPosts();
-  const series = await getEntry("Series", "ecosystem-architecture");
-  const description = series.data.summary;
+  const profile = await getEntry("Profiles", "yeehaa");
+  const description = profile.data.description
 
   return rss({
     title,
@@ -16,7 +20,12 @@ export async function GET(context: AstroUserConfig) {
     items: posts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.publishedAt,
+      author: post.data.author,
+      categories: post.data.series && [post.data.series.id],
       description: post.data.summary,
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+      }),
       link: `/posts/${post.slug}/`,
     }))
   });
