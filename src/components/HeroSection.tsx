7568c98c4d1e04002fa46e47396e46c35173fb3d
Jan Hein Hoogstad <yeehaa@offcourse.io>
2024-07-29 15:20:23 +0200

    better hero

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -11,11 +11,9 @@ type Props = {
   cta: CTA
 }
 
-export function HeroSection({ title, subtitle, cta, bannerImageURL }: Props) {
+export function HeroSection({ title, subtitle, cta }: Props) {
   return (
-    // <header className="relative h-screen lg:h-auto lg:w-full lg:aspect-[16/9] flex items-center justify-center bg-cover bg-center"
-    //   style={{ backgroundImage: `url(${bannerImageURL})` }}>
-    <header className="relative h-4/6 p-36 xl:p-48 flex items-center justify-center bg-cover bg-center">
+    <header className="relative h-4/6 pt-36 xl:pt-48 flex items-center justify-center bg-cover bg-center">
       <div className="absolute inset-0 bg-primary opacity-100"></div>
       <div className="relative z-10 text-center text-black px-4">
         <h1 className="text-6xl font-extrabold mb-6">{title}</h1>
@@ -29,5 +27,6 @@ export function HeroSection({ title, subtitle, cta, bannerImageURL }: Props) {
           <a href={cta.action}>{cta.text}</a>
         </Button>
       </div>
-    </header>)
+    </header>
+  )
 }
