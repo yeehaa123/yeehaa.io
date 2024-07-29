f8f200e5b6ade5305b30221ff3ce3bf9aa0c2318
Jan Hein Hoogstad <yeehaa@offcourse.io>
2024-07-29 07:39:43 +0200

    wave back

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -13,13 +13,14 @@ type Props = {
 
 export function HeroSection({ title, subtitle, cta, bannerImageURL }: Props) {
   return (
-    <header className="relative h-screen lg:h-auto lg:w-full lg:aspect-[16/9] flex items-center justify-center bg-cover bg-center"
-      style={{ backgroundImage: `url(${bannerImageURL})` }}>
-      <div className="absolute inset-0 bg-black opacity-80"></div>
-      <div className="relative z-10 text-center text-white px-4">
+    // <header className="relative h-screen lg:h-auto lg:w-full lg:aspect-[16/9] flex items-center justify-center bg-cover bg-center"
+    //   style={{ backgroundImage: `url(${bannerImageURL})` }}>
+    <header className="relative h-4/6 p-36 xl:p-48 flex items-center justify-center bg-cover bg-center">
+      <div className="absolute inset-0 bg-primary opacity-100"></div>
+      <div className="relative z-10 text-center text-black px-4">
         <h1 className="text-6xl font-extrabold mb-6">{title}</h1>
         <p className="text-2xl mb-10 max-w-2xl mx-auto">{subtitle}</p>
-        <Button size="lg" variant="outline" className="bg-black/0 rounded-none border-primary text-white font-bold text-lg hover:bg-white/80 hover:text-black transition-colors" asChild>
+        <Button size="lg" variant="outline" className="bg-black/0 rounded-none border-white text-white font-bold text-lg hover:bg-white/80 hover:text-black transition-colors" asChild>
           <a href={cta.action}>{cta.text}</a>
         </Button>
       </div>
