30ead444d8581471f3611ed30f922caf82a62b9a
Jan Hein Hoogstad <yeehaa@offcourse.io>
2024-07-30 08:38:46 +0200

    above fold

--- src/components/ApproachPreview.tsx
+++ src/components/ApproachPreview.tsx
@@ -1,8 +1,7 @@
 import {
   Compass, GitBranch, Brain, Waves,
-  Network, Layers, Boxes, ArrowRight, ArrowDown
+  Network, Layers, Boxes
 } from 'lucide-react';
-import { Fragment } from 'react/jsx-runtime';
 
 type Item = {
   title: string,
@@ -24,24 +23,19 @@ const IconMap = {
 };
 
 export const ApproachPreview = ({ items }: Props) => (
-  <div className="flex flex-col sm:flex-row justify-center items-center space-y-4 sm:space-y-0 sm:space-x-4 my-8">
-
-    {items.map((item, index) => {
-      // @ts-ignore
-      const Icon = IconMap[item.icon] || Boxes;
-      return <Fragment key={item.title}>
-        <div className="text-center flex flex-col items-center">
-          <Icon classNam="w-12 h-12 mb-2" />
-          <h3 className="font-semibold text-sm">{item.title}</h3>
-        </div>
-        {index < items.length - 1 && (
-          <>
-            <ArrowDown className="w-6 h-6 my-2 sm:hidden" />
-            <ArrowRight className="hidden sm:block w-6 h-6 mx-2" />
-          </>
-        )}
-      </Fragment>
-    })}
-  </div>
+  <section className="w-full pt-24 pb-24 md:pb-36 lg:pb-48">
+    <div className="container grid items-center justify-center gap-4 px-4 text-center md:px-6 lg:gap-10">
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-3 md:flex md:justify-center md:gap-12">
+        {items.map((item, index) => {
+          // @ts-ignore
+          const Icon = IconMap[item.icon] || Boxes;
+          return <div key={index} className="text-center flex flex-row gap-4">
+            <Icon classNam="w-12 h-12 mb-2" />
+            <h3 className="font-serif text-lg"><span className="font-extrabold">Step {index + 1}: </span>{item.title}</h3>
+          </div>
+        })}
+      </div>
+    </div>
+  </section>
 );
 
