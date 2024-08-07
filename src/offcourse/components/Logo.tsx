c69923527694d68d9e5f4bcfd05c4645dbd2fa6e
Jan Hein Hoogstad <yeehaa@offcourse.io>
2024-08-07 15:29:09 +0200

    slowly getting closer

--- src/offcourse/components/Logo.tsx
+++ src/offcourse/components/Logo.tsx
@@ -6,7 +6,8 @@ type Props = {
 }
 
 export function Logo({ className, onClick }: Props) {
-  return <svg className={cn("w-4 h-4 bg-white", className)} onClick={onClick || console.log} viewBox="0 0 180 180">
-    <path d="M0,0v180h180V0H0z M150,150H30v-30h120V150z" />
+  return <svg className={cn("w-4 h-4 bg-white", className)}
+    onClick={onClick || console.log} viewBox="0 0 180 180">
+    <path d="M0,0v181h181V0H0z M150,150H30v-30h120V150z" />
   </svg>
 }
