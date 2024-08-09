661db1b410b1e1344a7ba183879a1dc4cb87f126
Jan Hein Hoogstad <yeehaa@offcourse.io>
2024-08-09 12:23:54 +0200

    link about

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -8,7 +8,7 @@ export type NavItemProps = {
 export function NavItem({ title, href, className }: NavItemProps) {
   return (
     <li key={href}
-      className={cx("flex items-center gap-3 rounded-md py-2 text-md font-bold transition-colors hover:text-secondary data-[active=true]:text-secondary-dark", className)}>
+      className={cx("flex items-center gap-3 rounded-md py-2 text-md text-2xl font-bold text-offblack dark:text-offwhite transition-colors hover:text-secondary data-[active=true]:text-secondary-dark", className)}>
       <a href={href}>{title}</a>
     </li>)
 }
