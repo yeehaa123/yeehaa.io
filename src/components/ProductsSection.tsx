8f7a516e267711efd9478b9d30be629043bbb268
Jan Hein Hoogstad <yeehaa@offcourse.io>
2024-08-14 16:50:53 +0200

    more input

--- src/components/ProductsSection.tsx
+++ src/components/ProductsSection.tsx
@@ -19,13 +19,13 @@ export function ProductsSection({ title, subtitle, description, items, cta }: Pr
   return (
     <PageSection.Root>
       <PageSection.Container
-        className="flex flex-col justify-center items-center gap-y-12 lg:text-center">
+        className="flex flex-col justify-center items-center gap-y-12 lg:gap-16 lg:text-center">
         <div>
           <PageSection.Header>{title}</PageSection.Header>
           <PageSection.SubHeader >{subtitle}</PageSection.SubHeader>
           <PageSection.Description >{description}</PageSection.Description>
         </div>
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 w-full">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-5 w-full">
           {items.map((product, index) => <FeatureCard key={index} className="border-none bg-offwhite dark:bg-offblack" iconSize="LARGE" {...product} />)}
         </div>
         <div className="text-center mt-12">
